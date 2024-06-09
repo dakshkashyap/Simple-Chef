69e655f1c20ea325f0505ed559145ca80a2dc459
Daksh Kashyap <65839160+dakshkashyap@users.noreply.github.com>
2024-06-08 19:30:05 -0700

    fixed styling

--- src/components/recipelist/recipelist.jsx
+++ src/components/recipelist/recipelist.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import './recipelist.scss';
+import React from "react";
+import "./recipelist.scss";
 
 const RecipeList = ({ recipes }) => {
   return (
     <div className="recipe-list">
+      <div className="recipe-list__count">
+        {recipes.length} {recipes.length === 1 ? "recipe" : "recipes"} found
+        matching your ingredients
+      </div>
       {recipes.length ? (
         recipes.map((recipe) => (
           <div key={recipe.id} className="recipe-list__item">
-            <img src={recipe.image_path} alt={recipe.title} className="recipe-list__image" />
+            <div className="recipe-list__image-container">
+              <img
+                src={recipe.image_path}
+                alt={recipe.title}
+                className="recipe-list__image"
+              />
+            </div>
             <div className="recipe-list__details">
               <h3>{recipe.title}</h3>
               <p>{recipe.description}</p>
